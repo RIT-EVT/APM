5c459e379fd147618dda72b81160c7b127f22bb7
AarC10 <aarchan108@gmail.com>
2022-04-11 21:05:02 -0400

    Remove comment for source file

--- src/APM/dev/LTC2965IMS.cpp
+++ src/APM/dev/LTC2965IMS.cpp
@@ -12,12 +12,6 @@ namespace APM::DEV {
         this->_gpio = gpio;
     }
 
-    /**
-     * GPIO pin on MCU. PC6 is connected to output of IC
-     * Have as GPIO object in code
-     * EVT Core has gpio read pin.
-     * Read the pin and tell high or low return
-     */
     EVT::core::IO::GPIO::State LTC2965IMS::checkLogicLevel() {
         return _gpio.readPin();
     }
