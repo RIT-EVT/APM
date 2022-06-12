c010825c9049be357e549e074410d14daaaacebd
Aaron Chan <aarchan108@gmail.com>
2022-06-12 13:27:41 -0400

    Third times the charm

--- src/APM/APMUart.cpp
+++ src/APM/APMUart.cpp
@@ -11,23 +11,25 @@ APMUart::APMUart(IO::UART *apmUart, bool apmDebugPrint) {
     this -> apmDebugPrint = apmDebugPrint;
 }
 
+// clang-format off
 void APMUart::startupMessage() const {
     apmUart->printf("%s\n\r", MINICOM_CLEAR_DISPLAY);    // Escape sequence for minicom terminal to clear display
 
-    apmUart->printf("                       @@@@@@@@@@@@@@@@@@@@@@@@@@                      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\r"); // NOLINT
-    apmUart->printf("                      @@@@@@@@@@@@@@@@@@@@@@@@@@                     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n\r"); // NOLINT
-    apmUart->printf("                    (@@@@@@                                                     @@@@@@              \n\r"); // NOLINT
-    apmUart->printf("        ////       @@@@@@                                                      @@@@@@               \n\r"); // NOLINT
-    apmUart->printf("      //*//       @@@@@@                                                      @@@@@@                \n\r"); // NOLINT
-    apmUart->printf("  ////////////   @@@@@@@@@@@@@@@@@@   //////////*//         ////*/////////   @@@@@@   ./////////*/  \n\r"); // NOLINT
-    apmUart->printf("    ////*       @@@@@@                         /*///      //////            @@@@@@                  \n\r"); // NOLINT
-    apmUart->printf("   ////        @@@@@@                          /*///   //////             @@@@@@                    \n\r"); // NOLINT
-    apmUart->printf("              @@@@@@                           /*/*/ /*/*/*              @@@@@@                     \n\r"); // NOLINT
-    apmUart->printf("            @@@@@@@@@@@@@@@@@@@@@@@@@@@         *///////                @@@@@@                      \n\r"); // NOLINT
-    apmUart->printf("           @@@@@@@@@@@@@@@@@@@@@@@@@@@          */////                 @@@@@@                       \n\r"); // NOLINT
+    apmUart->printf("                       @@@@@@@@@@@@@@@@@@@@@@@@@@                      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\r");
+    apmUart->printf("                      @@@@@@@@@@@@@@@@@@@@@@@@@@                     @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ \n\r");
+    apmUart->printf("                    (@@@@@@                                                     @@@@@@              \n\r");
+    apmUart->printf("        ////       @@@@@@                                                      @@@@@@               \n\r");
+    apmUart->printf("      //*//       @@@@@@                                                      @@@@@@                \n\r");
+    apmUart->printf("  ////////////   @@@@@@@@@@@@@@@@@@   //////////*//         ////*/////////   @@@@@@   ./////////*/  \n\r");
+    apmUart->printf("    ////*       @@@@@@                         /*///      //////            @@@@@@                  \n\r");
+    apmUart->printf("   ////        @@@@@@                          /*///   //////             @@@@@@                    \n\r");
+    apmUart->printf("              @@@@@@                           /*/*/ /*/*/*              @@@@@@                     \n\r");
+    apmUart->printf("            @@@@@@@@@@@@@@@@@@@@@@@@@@@         *///////                @@@@@@                      \n\r");
+    apmUart->printf("           @@@@@@@@@@@@@@@@@@@@@@@@@@@          */////                 @@@@@@                       \n\r");
 
     apmUart->printf("\nDEV1 APMManager Initializing...\n\n\r");
 }
+// clang-format on
 
 
 void APMUart::setDebugPrint(bool debugPrint) {
